30d691d21ce3190b5b140a59ad5d8f844fda8191
Martin <mr.mmaurer+work@gmail.com>
2018-10-07 23:09:11 +0200

    Added Ajax more function

--- src/ListComponent/ListComponent.js
+++ src/ListComponent/ListComponent.js
@@ -10,21 +10,44 @@ class ListComponent extends Component {
 
     this.state = {
       entries: [],
+      isFetching: false,
+      startingPoint: 0,
+      limit: 3,
     };
+
+    this.moreButtonClicked = this.moreButtonClicked.bind(this);
   }
 
   componentDidMount() {
     // populate with dummy data
-    axios.get('http://jsonplaceholder.typicode.com/photos?_start=1&_limit=10')
-      .then(response => {
-        this.setState({
-          entries: (Array.isArray(response.data)) ? response.data : [response.data],
-        });
-    })
+    this.fetchData();
+  }
+
+  fetchData() {
+    const { startingPoint, limit } = this.state;
+
+    this.setState({ isFetching: true });
+    axios.get(`http://jsonplaceholder.typicode.com/photos?_start=${startingPoint}&_limit=${limit}`)
+      .then((response) => {
+        const fetchedEntries = (Array.isArray(response.data)) ? response.data : [response.data];
+
+        this.setState(oldState => ({
+          entries: oldState.entries.concat(fetchedEntries),
+          isFetching: false,
+        }));
+      })
+  }
+
+  moreButtonClicked() {
+    this.setState((oldState) => ({
+      startingPoint: oldState.startingPoint + oldState.limit,
+    }),() => {
+        this.fetchData();
+    });
   }
 
   render() {
-    const { entries } = this.state;
+    const { entries, isFetching } = this.state;
 
     const cards = entries.map((entry) =>
       <li className="list-entry list-entry--one-third" key={ entry.id }>
@@ -37,9 +60,11 @@ class ListComponent extends Component {
         <ul className="list">
           { cards }
         </ul>
-        <button className="button" disabled>
-          Load More
-        </button>
+        { entries.length > 0 &&
+          <button className="button button--more" onClick={ this.moreButtonClicked } disabled={ isFetching }>
+            Load more entries
+          </button>
+        }
       </section>
     )
   }
