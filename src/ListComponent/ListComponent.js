9df5277ad425bf3120b99175e37f25c9eb983977
Martin <mr.mmaurer+work@gmail.com>
2018-10-09 01:59:03 +0200

    Added basic imgur api stuff

--- src/ListComponent/ListComponent.js
+++ src/ListComponent/ListComponent.js
@@ -24,6 +24,32 @@ class ListComponent extends Component {
   }
 
   fetchData() {
+    const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = process.env;
+    // let url = "https://api.imgur.com/3/gallery/search/top/?q_all=derpy+cats&q_type=jpg";
+    // https://api.imgur.com/3/gallery/search/top/all/0/?q_all=derpy+cats&q_type=jpg
+    const url = 'imgur.json';
+
+    if (REACT_APP_CLIENT_ID === undefined || REACT_APP_CLIENT_SECRET === undefined) {
+      return
+    }
+
+    // this.setState({ isFetching: true });
+    axios.get(url,{ headers: { Authorization: `Client-ID ${REACT_APP_CLIENT_ID}`, crossdomain: true }})
+      .then((response) => {
+        console.log(response.data.data);
+        const fetchedEntries = (Array.isArray(response.data.data)) ? response.data.data : [response.data.data];
+
+        // filter out albums
+        // const fetchedSingleEntries = fetchedEntries.filter((entry) => entry['images_count'] === 1);
+
+        this.setState(oldState => ({
+          entries: oldState.entries.concat(fetchedEntries),
+          isFetching: false,
+        }));
+      })
+  }
+
+  fetchDataDummy() {
     const { startingPoint, limit } = this.state;
 
     this.setState({ isFetching: true });
@@ -51,7 +77,7 @@ class ListComponent extends Component {
 
     const cards = entries.map((entry) =>
       <li className="list-entry list-entry--one-third" key={ entry.id }>
-        <CardComponent title={ entry.title } image={ entry.url }></CardComponent>
+        <CardComponent title={ entry.title } image={ entry.link }></CardComponent>
       </li>
     );
 
